c117c14a24cda5f2aad3259c26ebfa97373986ca
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-07-22 14:50:46 +0700

    #30 Tích hợp thêm trang ForGot PassWord

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -3,4 +3,5 @@ const accountController = require('../../controller/admin/account.controller')
 
 router.get('/login', accountController.login)
 router.get('/register', accountController.register)
+router.get('/forgot-password', accountController.forgotPassword);
 module.exports = router;
