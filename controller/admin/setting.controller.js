6605750d015b10c870d9b1d3d314f21ff9e56a83
Nguyễn Trọng Thức <24110349@student.hcmute.edu.vn>
2026-07-22 17:51:17 +0700

    #38 Thiết lập giao diện tài khoản quản trị và tạo tài khoản Admin

--- controller/admin/setting.controller.js
+++ controller/admin/setting.controller.js
@@ -3,3 +3,21 @@ module.exports.list = async (req, res) => {
     pageTitle: "Cài đặt chung"
   })
 }
+
+module.exports.websiteInfo = async (req, res) => {
+  res.render('admin/pages/setting-website-info', {
+    pageTitle: "Thông tin website"
+  })
+}
+
+module.exports.accountAdminList = async (req, res) => {
+  res.render('admin/pages/setting-account-admin-list', {
+    pageTitle: "Tài khoản quản trị"
+  })
+}
+
+module.exports.accountAdminCreate = async (req, res) => {
+  res.render('admin/pages/setting-account-admin-create.pug', {
+    pageTitle: "Tài khoản quản trị"
+  })
+}
--- routes/admin/setting.route.js
+++ routes/admin/setting.route.js
@@ -2,5 +2,8 @@ const router = require('express').Router();
 const settingController = require('../../controller/admin/setting.controller.js')
 
 router.get('/list', settingController.list)
+router.get('/website-info', settingController.websiteInfo)
+router.get('/account-admin/list', settingController.accountAdminList)
+router.get('/account-admin/create', settingController.accountAdminCreate)
 
 module.exports = router;
